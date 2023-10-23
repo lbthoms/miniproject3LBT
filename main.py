3f3d0795a8ce9e300e437a85db73cb58607db2b3
lisa <lbthoms@mail.fhsu.edu>
2023-10-22 22:10:38 -0600

    Project requirements push

---
+++ main.py
@@ -0,0 +1,26 @@
+# INF601 - Advanced Programming in Python
+# Lisa Thoms
+# Mini Project 3
+
+
+#(5/5 points) Initial comments with your name, class and project at the top of your .py file.
+#(5/5 points) Proper import of packages used.
+#(70/70 points) Using Flask you need to setup the following:
+#(10/10 points) Setup a proper folder structure, use the tutorial as an example.
+#(20/20 points) You need to have a minimum of 5 pages, using a proper template structure.
+#(10/10 points) You need to have at least one page that utilizes a form and has the proper GET and POST
+# routes setup.
+#(10/10 points) You need to setup a SQLlite database with a minimum of two tables, linked with a
+# foreign key.
+#(10/10) You need to use Bootstrap in your web templates. I won't dictate exactly what modules you
+# need to use but the more practice here the better. You need to at least make use of a modal.
+#(10/10) You need to setup some sort of register and login system, you can use the tutorial as an
+# example.
+#(5/5 points) There should be a minimum of 5 commits on your project, be sure to commit often!
+#(5/5 points) I will be checking out the master branch of your project. Please be sure to include
+# a requirements.txt file which contains all the packages that need installed. You can create this
+# fille with the output of pip freeze at the terminal prompt.
+#(10/10 points) There should be a README.md file in your project that explains what your project
+# is, how to install the pip requirements, and how to execute the program. Please use the GitHub
+# flavor of Markdown. Be thorough on the explanations. You will need to explain the steps of
+# initializing the database and then how to run the development server for your project.
